de14df920a976ae0b67e6e9f04967c50ded19e50
ashikulislamcse <ashikulislamcse@gmail.com>
2025-02-28 19:02:37 +0600

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import ProductCard from './components/PproductCard.jsx'
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <ProductCard/>
+    </>
   )
 }
 
